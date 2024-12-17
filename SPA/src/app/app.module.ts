ca456f96490cf18b1b4c899afdb6aa91ee565a02
Erik <coutinhoerik@gmail.com>
2024-12-16 21:53:15 -0300

    Adicionando a pasta SPA ao repositório principal

---
+++ SPA/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
